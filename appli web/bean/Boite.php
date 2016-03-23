dbb13345ac0687ef65f42b3484f06e7ab387122f
Gwendoline LESVIER <E145620H@U-INF-J-E113-22.iut-nantes.univ-nantes.prive>
2016-03-23 10:05:11 +0100

    Blop ?

---
+++ appli web/bean/Boite.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* 
+*/
+class Boite
+{
+	
+	function __construct($NomAct, $NomC, $NomInstaltion, $NumeroFiche)
+	{
+		# code...
+		$this ->NomActivite = $NomAct;
+		$this ->NomCommune = $NomC;
+		$ 
+	}
+}
+?>
